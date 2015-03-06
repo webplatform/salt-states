e99721ac063f75d0b4f48b1a4eb6bd746faf2bfc
Renoir Boulanger <renoir@w3.org>
2015-03-06 16:29:51 -0500

    human readable helper command to know how much memory a process uses

--- webplatform/files/wpd_aliases.sh
+++ webplatform/files/wpd_aliases.sh
@@ -9,6 +9,10 @@ alias wpd-service-test='nc -vz '
 alias wpd-netstat='netstat -tulpn'
 alias wpd-gentoken='openssl rand -base64 32'
 
+wpd-memusage () {
+  ps -ylC "$@" --sort:rss | awk '!/RSS/ { s+=$8 } END { printf "%s\n", "Total memory used:"; printf "%dM\n", s/1024 }'
+}
+
 # ref: http://www.commandlinefu.com/commands/view/3543/show-apps-that-use-internet-connection-at-the-moment.
 wpd-lsof-network-services() {
   lsof -P -i -n | cut -f 1 -d " "| uniq | tail -n +2
